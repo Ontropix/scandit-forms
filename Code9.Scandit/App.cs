f15fb7590ac9a594e64ac2bd86de0cfc8da4e50d
andrei schneider <megaherz@bk.ru>
2014-08-14 17:42:44 +0300

    scan button

--- Code9.Scandit/App.cs
+++ Code9.Scandit/App.cs
@@ -7,11 +7,43 @@ namespace Code9.Scandit
 	{
 		public static Page GetMainPage ()
 		{	
+			Button scanButton = new Button() {
+				HorizontalOptions = LayoutOptions.Center,
+				Text = "Tap to Scan"
+			};
+
+			Label message = new Label () {
+				XAlign = TextAlignment.Center
+			};
+
+			IScanner scanner = DependencyService.Get<IScanner> ();
+
+			//Configure scanner. Please get your own Scandit key!
+			scanner.Configure ("xbENFiLaEeSCn/KdTNpva0lad8WlMjCpK89rxosdty4", "Cancel");
+
+			scanButton.Clicked += async (object sender, EventArgs e) => {
+
+				message.Text = string.Empty;
+
+				ScanResult result =  await scanner.ScanAsync();
+
+				if (result.Success) {
+					message.Text = string.Format("Success. Code = {0}", result.Code);
+				}
+				else {
+					message.Text = "Failed";
+				}
+			};
+
 			return new ContentPage { 
-				Content = new Label {
-					Text = "Hello, Forms!",
+				Content = new StackLayout() {
+					Padding = 5,
+					Spacing = 10,
 					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					Children = {
+						scanButton,
+						message
+					}
 				}
 			};
 		}
